ed929bc7e0bc752f3f1b3ad59d23c8bf9a6d9db8
Aman Choudhary <aman1567goal@gmail.com>
2017-11-12 15:59:12 +0530

    Songify project started

---
+++ main.js
@@ -0,0 +1,5 @@
+var button = document.querySelector('.welcome-screen button');
+button.addEventListener('click', function() {
+    var name = document.querySelector('#name-input').value;
+    console.log(name)
+});
