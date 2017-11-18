c06ee4f918acd34e3cda531f571840ffeb0d1eca
Aman Choudhary <aman1567goal@gmail.com>
2017-11-18 16:27:50 +0530

    showing time now

--- main.js
+++ main.js
@@ -1,4 +1,18 @@
 
+function toggleSong() {
+    var song = document.querySelector('audio');
+
+    if(song.paused == true) {
+        console.log('Playing');
+        $('.play-icon').removeClass('fa-play').addClass('fa-pause');
+        song.play();
+    }
+    else {
+        console.log('Pausing');
+        $('.play-icon').removeClass('fa-pause').addClass('fa-play');
+        song.pause();
+    }
+}
 
 $('.welcome-screen button').on('click', function() {
     var name = $('#name-input').val();
@@ -14,31 +28,55 @@ $('.welcome-screen button').on('click', function() {
     }
 });
 $('.play-icon').on('click',function() {
-    var song = document.querySelector('audio');
-    if(song.paused == true) {
-        console.log('Playing');
-        $('.play-icon').removeClass('fa-play').addClass('fa-pause');
-        song.play();
-    }
-    else {
-        console.log('Pausing');
-        $('.play-icon').removeClass('fa-pause').addClass('fa-play');
-        song.pause();
-    }
+    toggleSong()
 });
 $('body').on('keypress',function(event) {
     if (event.keyCode == 32){
-
-        var song = document.querySelector('audio');
-        if(song.paused == true) {
-            console.log('Playing');
-            $('.play-icon').removeClass('fa-play').addClass('fa-pause');
-            song.play();
-        }
-        else {
-            console.log('Pausing');
-            $('.play-icon').removeClass('fa-pause').addClass('fa-play');
-            song.pause();
-        }
+        toggleSong()
     }
 });
+
+function fancyTimeFormat(time)
+{
+    // Hours, minutes and seconds
+    var hrs = ~~(time / 3600);
+    var mins = ~~((time % 3600) / 60);
+    var secs = time % 60;
+
+    // Output like "1:01" or "4:03:59" or "123:03:59"
+    var ret = "";
+
+    if (hrs > 0) {
+        ret += "" + hrs + ":" + (mins < 10 ? "0" : "");
+    }
+
+    ret += "" + mins + ":" + (secs < 10 ? "0" : "");
+    ret += "" + secs;
+    return ret;
+}
+
+function updateCurrentTime() {
+    var song = document.querySelector('audio');
+    // console.log(song.currentTime);
+    // console.log(song.duration);
+    var currentTime = Math.floor(song.currentTime);
+    currentTime = fancyTimeFormat(currentTime);
+
+    var duration = Math.floor(song.duration);
+    duration = fancyTimeFormat(duration)
+    $('.time-elapsed').text(currentTime);
+    $('.song-duration').text(duration);
+}
+
+var songName = 'Tamma Song';
+
+window.onload = function() {
+    $('.song-name').text(songName);
+
+    updateCurrentTime();
+    setInterval(function() {
+        updateCurrentTime();
+    },1000);
+}
+
+
